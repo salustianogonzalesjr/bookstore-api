800e9b1d49eb8c395b7c0d02dc017219ed47f361
Sag <salustiano@aussiebum.com>
2022-12-10 19:28:22 +0800

    adding unit test

--- tests/Unit/BookTest.php
+++ tests/Unit/BookTest.php
@@ -2,9 +2,28 @@
 
 namespace Tests\Unit;
 
-use PHPUnit\Framework\TestCase;
+use App\Models\User;
+use Illuminate\Foundation\Testing\RefreshDatabase;
+use Illuminate\Foundation\Testing\WithFaker;
+// use PHPUnit\Framework\TestCase;
+use Tests\TestCase;
+
 
 class BookTest extends TestCase
 {
+    use RefreshDatabase;
+    use WithFaker;
+    
+    public function test_login_route_api()
+    {              
+        $user = User::factory()->create();
 
+        $hasUser = $user ? true : false;
+
+        $this->assertTrue($hasUser);
+
+        $response = $this->actingAs($user)->get('/api/v1/books');     
+        
+        $response->assertStatus(200);
+    }
 }
